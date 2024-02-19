87212d22dc8ddebcb477a36e746dce8ee28d4737
Rayaan Hussain <md.rayaan.h@gmail.com>
2024-02-19 15:34:17 +0000

    fix: exit on error

--- cmd/lfs/main.go
+++ cmd/lfs/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"lfs/internal"
+	"os"
 )
 
 func main() {
 	server, err := internal.CreateServer("localhost", 8080, "tcp")
 	if err != nil {
-		fmt.Println("Could not start server", err)
+		fmt.Printf("Could not start server: %s \n", err)
+		os.Exit(1)
 	}
 	server.AcceptConnections(internal.Engine)
 }
